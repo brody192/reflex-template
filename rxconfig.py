72121831f75dbb5394582b6721f3335d454d3e3e
Brody Over <brodyover@gmail.com>
2023-08-28 18:12:50 -0400

    t

--- rxconfig.py
+++ rxconfig.py
@@ -9,8 +9,8 @@ class ReflextemplateConfig(rx.Config):
 config = ReflextemplateConfig(
     app_name="myapp",
     telemetry_enabled=False,
-    frontend_port=3000,
-    backend_port=8000,
-    backend_host="0.0.0.0",
+    frontend_port=3000, # default frontend port
+    backend_port=8000, # default backend port
+    # use https and the railway public domain with a backend route if available, otherwise default to a local address
     api_url=f'https://{os.environ[railway_domain]}/backend' if railway_domain in os.environ else "http://127.0.0.1:8000"
 )
