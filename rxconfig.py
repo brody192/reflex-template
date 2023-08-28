d95ea4cc3672c88446e011b8dcda84576aa2d8c1
Brody Over <brodyover@gmail.com>
2023-08-28 00:57:44 -0400

    t

--- rxconfig.py
+++ rxconfig.py
@@ -1,6 +1,8 @@
 import reflex as rx
 import os
 
+print(f'https://{os.getenv("API_URL")}' if "API_URL" in os.environ else "http://127.0.0.1:8000")
+
 class ReflextemplateConfig(rx.Config):
     pass
 
@@ -10,5 +12,7 @@ config = ReflextemplateConfig(
     frontend_port=os.getenv("PORT", 3000),
     backend_port=os.getenv("PORT", 8000),
     backend_host="0.0.0.0",
-    api_url=os.getenv("API_URL", "http://127.0.0.1:8000")
+    api_url=f'https://{os.getenv("API_URL")}' if "API_URL" in os.environ else "http://127.0.0.1:8000"
 )
+
+# os.getenv("API_URL", "http://127.0.0.1:8000")
