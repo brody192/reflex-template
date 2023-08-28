81cd6d613eafacdb3e77a1460ecccbe92a040cee
Brody Over <brodyover@gmail.com>
2023-08-28 17:55:43 -0400

    t

--- rxconfig.py
+++ rxconfig.py
@@ -12,5 +12,5 @@ config = ReflextemplateConfig(
     frontend_port=3000,
     backend_port=8000,
     backend_host="0.0.0.0",
-    api_url=f'https://{os.getenv(railway_domain)}/backend' if railway_domain in os.environ else "http://127.0.0.1:8000"
+    api_url=f'https://{os.environ[railway_domain]}/backend' if railway_domain in os.environ else "http://127.0.0.1:8000"
 )
