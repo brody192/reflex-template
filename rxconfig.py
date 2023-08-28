2cb80e354562340d9180c4f5fd1b40b1f0246bcf
Brody Over <brodyover@gmail.com>
2023-08-28 01:01:50 -0400

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,7 +1,11 @@
 import reflex as rx
 import os
 
-print(f'https://{os.getenv("API_URL")}' if "API_URL" in os.environ else "http://127.0.0.1:8000")
+railway_domain = "RAILWAY_PUBLIC_DOMAIN"
+
+url = f'https://{os.getenv(railway_domain)}' if railway_domain in os.environ else "http://127.0.0.1:8000"
+
+print(url)
 
 class ReflextemplateConfig(rx.Config):
     pass
@@ -12,7 +16,7 @@ config = ReflextemplateConfig(
     frontend_port=os.getenv("PORT", 3000),
     backend_port=os.getenv("PORT", 8000),
     backend_host="0.0.0.0",
-    api_url=f'https://{os.getenv("API_URL")}' if "API_URL" in os.environ else "http://127.0.0.1:8000"
+    api_url=f'https://{os.getenv(railway_domain)}' if railway_domain in os.environ else "http://127.0.0.1:8000"
 )
 
 # os.getenv("API_URL", "http://127.0.0.1:8000")
